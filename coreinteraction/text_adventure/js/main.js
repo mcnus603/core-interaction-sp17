7632a9c7b1919b9c237b3cd18dfad657d501c6b4
mcnus603 <mcnus603@newschool.edu>
2017-02-16 13:01:06 -0500

    update

--- coreinteraction/text_adventure/js/main.js
+++ coreinteraction/text_adventure/js/main.js
@@ -56,13 +56,297 @@ $(document).ready(function() {
 		$(".sleeping_beauty_page").show();
 	});
 
+//homepage return 
 	$(".bookmark").click(function(e){
 		$(".homepage").show();
 		$(".sleeping_beauty_page").hide();
 		$(".rapunzel_page").hide();
 		$(".cinderella_page").hide();
 		$(".snow_white_page").hide();
-	})
+		$(".snow_white_page").css("width","500px");
+		$(".sleeping_beauty_page").css("width","500px");
+		$(".cinderella_page").css("width","500px");
+		$(".rapunzel_page").css("width","500px");
+
+		$(".snow_white_page").css("top","100px");
+		$(".sleeping_beauty_page").css("top","100px");
+		$(".cinderella_page").css("top","100px");
+		$(".rapunzel_page").css("top","100px");
+
+		$(".snow_white_page").css("left","150px");
+		$(".sleeping_beauty_page").css("left","150px");
+		$(".cinderella_page").css("left","150px");
+		$(".rapunzel_page").css("left","150px");
+	
+		$(".sleeping_beauty_page").mouseenter(function(){
+			$(".sleeping_beauty_title").css("font-size","30px");
+		});
+
+		$(".rapunzel_page").mouseenter(function(){
+			$(".rapunzel_title").css("font-size","30px");
+		});
+
+		$(".cinderella_page").mouseenter(function(){
+			$(".cinderella_title").css("font-size","30px");
+		});
+		$(".snow_white_page").mouseenter(function(){
+			$(".snow_white_title").css("font-size","30px");
+		});
+		
+	});
+
+	$(".once_upon_a_time").click(function(e){
+		$(".homepage").show();
+		$(".sleeping_beauty_page").hide();
+		$(".rapunzel_page").hide();
+		$(".cinderella_page").hide();
+		$(".snow_white_page").hide();
+		$(".snow_white_page").css("width","500px");
+		$(".sleeping_beauty_page").css("width","500px");
+		$(".cinderella_page").css("width","500px");
+		$(".rapunzel_page").css("width","500px");
+
+		$(".snow_white_page").css("top","100px");
+		$(".sleeping_beauty_page").css("top","100px");
+		$(".cinderella_page").css("top","100px");
+		$(".rapunzel_page").css("top","100px");
+
+		$(".snow_white_page").css("left","150px");
+		$(".sleeping_beauty_page").css("left","150px");
+		$(".cinderella_page").css("left","150px");
+		$(".rapunzel_page").css("left","150px");
+
+		$(".sleeping_beauty_page").mouseenter(function(){
+			$(".sleeping_beauty_title").css("font-size","30px");
+		});
+
+		$(".rapunzel_page").mouseenter(function(){
+			$(".rapunzel_title").css("font-size","30px");
+		});
+
+		$(".cinderella_page").mouseenter(function(){
+			$(".cinderella_title").css("font-size","30px");
+		});
+		$(".snow_white_page").mouseenter(function(){
+			$(".snow_white_title").css("font-size","30px");
+		});
+
+
+	});
+
+	$(".line").mouseenter(function(){
+		$(this).css("color", colors[Math.floor(Math.random()*colors.length)]);
+	});
+
+//toogle links 
+
+//rapunzel
+	$(".rapunzel_title").click(function(){
+		$(".sleeping_beauty_page").hide();
+		$(".cinderella_page").hide();
+		$(".snow_white_page").hide();
+
+		$(".snow_white_page").css("width","500px");
+		$(".sleeping_beauty_page").css("width","500px");
+		$(".cinderella_page").css("width","500px");
+		$(".rapunzel_page").css("width","500px");
+
+		$(".snow_white_page").css("top","100px");
+		$(".sleeping_beauty_page").css("top","100px");
+		$(".cinderella_page").css("top","100px");
+		$(".rapunzel_page").css("top","100px");
+
+		$(".snow_white_page").css("left","150px");
+		$(".sleeping_beauty_page").css("left","150px");
+		$(".cinderella_page").css("left","150px");
+		$(".rapunzel_page").css("left","150px");
+
+		$(".rapunzel_page").mouseenter(function(){
+			$(".rapunzel_title").css("font-size","30px");
+		});
+
+	});
+//sleepingbeauty
+	$(".sleeping_beauty_title").click(function(){
+		$(".rapunzel_page").hide();
+		$(".cinderella_page").hide();
+		$(".snow_white_page").hide();
+
+		$(".snow_white_page").css("width","500px");
+		$(".sleeping_beauty_page").css("width","500px");
+		$(".cinderella_page").css("width","500px");
+		$(".rapunzel_page").css("width","500px");
+
+		$(".snow_white_page").css("top","100px");
+		$(".sleeping_beauty_page").css("top","100px");
+		$(".cinderella_page").css("top","100px");
+		$(".rapunzel_page").css("top","100px");
+
+		$(".snow_white_page").css("left","150px");
+		$(".sleeping_beauty_page").css("left","150px");
+		$(".cinderella_page").css("left","150px");
+		$(".rapunzel_page").css("left","150px");
+
+		$(".sleeping_beauty_page").mouseenter(function(){
+			$(".sleeping_beauty_title").css("font-size","30px");
+		});
+
+	});
+
+	//cinderella
+
+	$(".cinderella_title").click(function(){
+		$(".rapunzel_page").hide();
+		$(".sleeping_beauty_page").hide();
+		$(".snow_white_page").hide();
+
+		$(".snow_white_page").css("width","500px");
+		$(".sleeping_beauty_page").css("width","500px");
+		$(".cinderella_page").css("width","500px");
+		$(".rapunzel_page").css("width","500px");
+
+		$(".snow_white_page").css("top","100px");
+		$(".sleeping_beauty_page").css("top","100px");
+		$(".cinderella_page").css("top","100px");
+		$(".rapunzel_page").css("top","100px");
+
+		$(".snow_white_page").css("left","150px");
+		$(".sleeping_beauty_page").css("left","150px");
+		$(".cinderella_page").css("left","150px");
+		$(".rapunzel_page").css("left","150px");
+
+		$(".cinderella_page").mouseenter(function(){
+			$(".cinderella_title").css("font-size","30px");
+		});
+
+	});
+
+	//snow white
+	$(".snow_white_title").click(function(){
+		$(".rapunzel_page").hide();
+		$(".sleeping_beauty_page").hide();
+		$(".cinderella_page").hide();
+
+		$(".snow_white_page").css("width","500px");
+		$(".sleeping_beauty_page").css("width","500px");
+		$(".cinderella_page").css("width","500px");
+		$(".rapunzel_page").css("width","500px");
+
+		$(".snow_white_page").css("top","100px");
+		$(".sleeping_beauty_page").css("top","100px");
+		$(".cinderella_page").css("top","100px");
+		$(".rapunzel_page").css("top","100px");
+
+		$(".snow_white_page").css("left","150px");
+		$(".sleeping_beauty_page").css("left","150px");
+		$(".cinderella_page").css("left","150px");
+		$(".rapunzel_page").css("left","150px");
+
+		$(".snow_white_page").mouseenter(function(){
+			$(".snow_white_title").css("font-size","30px");
+		});
+
+	});
+
+
+//childless sleeping beauty to rapunzel 
+
+	$(".childless_sleeping_beauty").click(function(e){
+		$(".rapunzel_page").show();
+		$(".sleeping_beauty_page").css("width","350px");
+		$(".rapunzel_page").css("width","350px");
+		$(".rapunzel_page").css("left","600px");
+		$(".childless_rapunzel").css("color", colors[Math.floor(Math.random()*colors.length)]);
+		
+		//hover state
+		$(".rapunzel_page").mouseenter(function(){
+			$(".rapunzel_title").css("font-size","45px");
+		});
+		$(".rapunzel_page").mouseleave(function(){
+			$(".rapunzel_title").css("font-size","30px");
+		});
+		$(".sleeping_beauty_page").mouseenter(function(){
+			$(".sleeping_beauty_title").css("font-size","45px");
+		});
+		$(".sleeping_beauty_page").mouseleave(function(){
+			$(".sleeping_beauty_title").css("font-size","30px");
+		});
+
+	});
+
+//childless rapunzel to sleeping
+
+	$(".childless_rapunzel").click(function(e){
+		$(".sleeping_beauty_page").show();
+		$(".sleeping_beauty_page").css("width","350px");
+		$(".rapunzel_page").css("width","350px");
+		$(".sleeping_beauty_page").css("left","600px");
+		$(".childless_sleeping_beauty").css("color", colors[Math.floor(Math.random()*colors.length)]);
+		
+		//hover state
+		$(".rapunzel_page").mouseenter(function(){
+			$(".rapunzel_title").css("font-size","45px");
+		});
+		$(".rapunzel_page").mouseleave(function(){
+			$(".rapunzel_title").css("font-size","30px");
+		});
+		$(".sleeping_beauty_page").mouseenter(function(){
+			$(".sleeping_beauty_title").css("font-size","45px");
+		});
+		$(".sleeping_beauty_page").mouseleave(function(){
+			$(".sleeping_beauty_title").css("font-size","30px");
+		});
+
+	});
+//seven sleeping beauty to snow white
+	$(".seven_sleeping_beauty").click(function(e){
+		$(".snow_white_page").show();
+		$(".sleeping_beauty_page").css("width","350px");
+		$(".snow_white_page").css("width","350px");
+		$(".snow_white_page").css("left","600px");
+		$(".seven_snow_white").css("color", colors[Math.floor(Math.random()*colors.length)]);
+		
+		//hover state
+		$(".snow_white_page").mouseenter(function(){
+			$(".snow_white_title").css("font-size","45px");
+		});
+		$(".snow_white_page").mouseleave(function(){
+			$(".snow_white_title").css("font-size","30px");
+		});
+		$(".sleeping_beauty_page").mouseenter(function(){
+			$(".sleeping_beauty_title").css("font-size","45px");
+		});
+		$(".sleeping_beauty_page").mouseleave(function(){
+			$(".sleeping_beauty_title").css("font-size","30px");
+		});
+
+	});
+
+//seven snow white to sleeping beauty 
+	$(".seven_snow_white").click(function(e){
+		$(".sleeping_beauty_page").show();
+		$(".sleeping_beauty_page").css("width","350px");
+		$(".snow_white_page").css("width","350px");
+		$(".sleeping_beauty_page").css("left","600px");
+		$(".seven_sleeping_beauty").css("color", colors[Math.floor(Math.random()*colors.length)]);
+		
+		//hover state
+		$(".snow_white_page").mouseenter(function(){
+			$(".snow_white_title").css("font-size","45px");
+		});
+		$(".snow_white_page").mouseleave(function(){
+			$(".snow_white_title").css("font-size","30px");
+		});
+		$(".sleeping_beauty_page").mouseenter(function(){
+			$(".sleeping_beauty_title").css("font-size","45px");
+		});
+		$(".sleeping_beauty_page").mouseleave(function(){
+			$(".sleeping_beauty_title").css("font-size","30px");
+		});
+
+	});
+
+
 
 
 

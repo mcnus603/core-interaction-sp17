ca881c1a3e76ad3dda39810984c6acdf637f4ab4
Sarah McNutt <mcnus603@newschool.edu>
2017-04-25 16:46:08 -0400

    reading response

---
+++ coreinteraction/collection/collection.js
@@ -0,0 +1,26 @@
+var wholeSet = [    
+{
+	"word": "Atavastic", 
+	"illustrations": {
+		"single": "path/to/img.jpg",
+		"multiple": "path/to/img.jpg"
+	}
+	"wordClass" : "adjective"
+	"definition" : "jisoadioadjoa"
+	"quote": "hsuaidhsd"
+	"pronunciation" : {
+		""
+	}
+},
+{},
+{}
+
+
+
+
+
+
+];
+
+
+
---
+++ coreinteraction/collection/main.js
@@ -0,0 +1,14 @@
+//underscore library to filter
+
+var letterBox = document.getElementsByClassName("letterBox");
+var letterBoxWidth = letterBox.clientWidth; 
+
+console.log(letterBoxWidth);
+
+// document.onload
+
+
+
+
+
+

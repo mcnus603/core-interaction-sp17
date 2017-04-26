77dee04b8f4c4796ef93dc837fea38b2cd42e7b9
Sarah McNutt <mcnus603@newschool.edu>
2017-04-26 01:29:17 -0400

    creative compute

--- coreinteraction/collection/collection.js
+++ coreinteraction/collection/collection.js
@@ -1,6 +1,24 @@
 var wholeSet = [    
 {
-	"word": "Atavastic", 
+	"word":"atavastic", 
+
+	"illustrations": {
+		"single": "path/to/img.jpg",
+		"multiple": "path/to/img.jpg",
+	}
+
+	"wordClass" : "adjective",
+
+	"definition" : "relating to or characterized by reversion to something ancient or ancestral.",
+
+	"quote": "hsuaidhsd",
+	"page": "257",
+	"pronunciation" : {
+		""
+	}
+},
+{
+	"word":"atavastic", 
 	"illustrations": {
 		"single": "path/to/img.jpg",
 		"multiple": "path/to/img.jpg"
@@ -11,9 +29,15 @@ var wholeSet = [
 	"pronunciation" : {
 		""
 	}
+
+
 },
-{},
-{}
+
+
+{
+
+
+}
 
 
 

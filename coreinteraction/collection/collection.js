70daff4dfdaf1c7a4050f20f0928dac247850c16
Sarah McNutt <mcnus603@newschool.edu>
2017-04-28 01:07:09 -0400

    collection

--- coreinteraction/collection/collection.js
+++ coreinteraction/collection/collection.js
@@ -1,45 +1,144 @@
-var wholeSet = [    
+var collection = [    
 {
+	"letternumber": "a1", 
 	"word":"atavastic", 
-
-	"illustrations": {
-		"single": "path/to/img.jpg",
-		"multiple": "path/to/img.jpg",
-	}
-
 	"wordClass" : "adjective",
-
 	"definition" : "relating to or characterized by reversion to something ancient or ancestral.",
+	"quote": "Uninitiated adults who might be parked in a nearby mint-green advertorial Ford sedan or might stroll casually past E.T.A.'s four easternmost tennis courts and see an atavistic global-nuclear-conflict game played by tanned and energetic little kids and so this might naturally expect to see fuzzless green warheads getting whacked indiscriminately skyward all over the place as everyone gets blackly drunk with thanatoptic fury in the crisp November air--these adults would more likely find an actual game of Eschaton strangely subdued, almost narcotized-looking.",
+	"page": 257,
 
-	"quote": "hsuaidhsd",
-	"page": "257",
-	"pronunciation" : {
-		""
-	}
+
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
 },
 {
-	"word":"atavastic", 
-	"illustrations": {
-		"single": "path/to/img.jpg",
-		"multiple": "path/to/img.jpg"
-	}
-	"wordClass" : "adjective"
-	"definition" : "jisoadioadjoa"
-	"quote": "hsuaidhsd"
-	"pronunciation" : {
-		""
-	}
+	"letternumber": "b1",
+	"word":"brisance", 
+	"wordClass" : "noun",
+	"definition" : "the shattering or crushing effect of a sudden release of energy as in an explosion",
+	"quote": "Randy Lenz found that if he could get an urban cat up close enough with some outstretched tuna he could pop the Hefty bag over it and scoop up from the bottom so the cat was in the air in the bottom of the bag, and then he could tie the bag shut with the complimentary wire twist-tie that comes with each bag. He could put the closed bag down next to the vicinity’s northernmost wall or fence or dumpster and light a gasper and hunker down up next to the wall and watch the wide variety of changing shapes the bag would assume as the agitated cat got lower on air. The shapes got more and more violent and twisted in mid-air with the passage of a minute. After it stopped assuming shapes Lenz would dab his butt with a spitty finger to save the rest for later and get up and untie the twist-tie and look inside the bag and go: ‘There’. The ‘There’ turned out to be crucial for the sense of brilliance and closure and resolving issues of impotent rage and powerless fear that like accord in Lenz all day being trapped in the northeaster portions of a squalid halfway house all day fearing for his life, Lenz felt. ",
+	"page":541,
 
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
+
+
+},
+
+{
+	"letternumber": "c1",
+	"word":"coruscant", 
+	"wordClass" : "adjective",
+	"definition" : "giving forth flashes of light; glittering; sparkling",
+	"quote": "…the NASA blanket blowing away from the scooped engineer’s failing form about halfway down and suddenly aloft in a hillside thermal and blown far out over Arlington St. traffic by the keen November wind, the madly squeaking wheelchair aloft over hillside moguls and coming back down and up again, the snatched engineer in the chair’s scoop appearing to the hillside’s roused figures mostly hallucinatory waggle of bare limbs and strangely wheezy shrieks for Help or at least to Look Out Below, all as the modified chair squeaks frantically straight down the hillside’s most efficient downward line toward the van with the ramp now idling in gear, its pipe’s exhaust beating the street in high-rpm idle, the NASA blanket twisting coruscant in the air high above the street…",
+	"page":626,
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
+
+},
+
+{
+	"letternumber": "d1",
+	"word":"diaphoretic", 
+	"wordClass" : "noun",
+	"definition" : "perspiring profusely",
+	"quote": "All ye peronic or teratoidal. The phrenologically malformed. The suppuratively lesioned. The endocrinological malodorous or whatever ilk. Run don’t walk on down. The acrevulvus-nosed. The radically ectomied. The morbidly diaphoretic with a hankie in every pocket. The chronically granulomatous. ",
+	"page":190,
+
+
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
 
 },
 
 
 {
+	"letternumber": "e1",
+	"word":"erumpent", 
+	"wordClass" : "adjective",
+	"definition" : "tending to grow out vigorously from a substrate so as to burst through or rise above its surface; grown or burst through a surface (as of a host's tissue) so as to form a projecting mass",
+	"quote":"Hal Incandenza is now being encouraged to identify himself as a late-blooming prodigy and possible genius at tennis who is on the verge of making every authority-figure in his world and beyond very proud indeed. He’s never looked better on court or on monthly O.N.A.N.T.A. paper. He is erumpent. He has made what Schtitt termed a ‘leap of exponents’ at a post-pubescent age when radical, plateaux-hopping, near-J.-Wayne-and-Show-caliber improvement is extraordinarily rare in tennis. ",
+	"page":155,
 
+
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
+
+},
+
+{
+	"letternumber": "f1",
+	"word":"ferbile", 
+	"wordClass" : "adjective",
+	"definition" : "having or showing the symptoms of a fever; having or showing a great deal of nervous excitement or energy",
+	"quote":"…she is not drunk Joelle now sees but has taken ecstasy, Joelle can see, from the febrile flush and eyes jacked so wide you can make out brain-meat behind the balls’ poles, a.k.a. X or MDMA, a beta-something, an early synthetic, emotional acid, the Love Drug so-called, big among the artistic young under say Bush and successors, since fallen into relative disuse because its pulverizing hangover has been linked to the impulsive use of automatic weapons in public venues, a hangover that makes a freebase hangover look like a day at the emotional beach, the difference between suicide and homicide consisting perhaps only in where you think you discern the cage’s door: Would she kill somebody else to get out of the cage? ",
+	"page":854,
+
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
+
+},
+
+
+{
+	"letternumber": "g1",
+	"word":"greensward", 
+	"wordClass" : "noun",
+	"definition" : "grass-covered ground; turf; an area of this",
+	"quote":"The Gardens’ only other slept is now the bowl of the former pond. The hillside’s grassy decline, not too steep, runs at a wedge’s angle down toward Arlington St. and is one broad greensward, free of dog droppings because dogs won’t go to the bathroom on inclined terrain. ",
+	"page":623,
+
+
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
+},
+
+
+{
+	"letternumber": "h1",
+	"word":"halcyon", 
+	"wordClass" : "adjective",
+	"definition" : "denoting a period of time in the past that was idyllically happy andperous, happy",
+	"quote":"He confessed to Lyle one night in the quiet weight room that he’d gone in for high-level athletics mostly to have an excuse of some sort for being as wet as he was. It always looked like Marlon had been rain on. But it wasn’t rain. It’s like Marlon hadn’t been dry since the womb. It’s like he leaked. It had been a tormentingly but also in certain ways halcyon few years, in the past. A tormentingly unspecific hope in the air. Lyle had told this boy everything he had to tell. ",
+	"page":218,
+
+
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
+
+},
+
+{
+	"letternumber": "i1",
+	"word":"intalioed", 
+	"wordClass" : "verb",
+	"definition" : "to engrave with a sunk pattern or design; to represent or execute in intaglio",
+	"quote":" A nonpeanut M&M box is intaglioed into the concrete of the sidewalk under Green, so bleached by the elements it’s turned bone-white and is only barely identifiable as a nonpeanut M&M box. ",
+	"page":583,
+
+
+
+	"pronunciation" :"",
+	"single": "",
+	"whole": "",
 
 }
-
-
 
 
 
--- coreinteraction/collection/main.js
+++ coreinteraction/collection/main.js
@@ -1,14 +1,167 @@
-//underscore library to filter
+var alphabet = ['A', 'B','C','D','E', 'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D']
+var container = document.getElementById("container");
 
-var letterBox = document.getElementsByClassName("letterBox");
-var letterBoxWidth = letterBox.clientWidth; 
-
-console.log(letterBoxWidth);
-
-// document.onload
-
-
-
+for (var i = 1; i < 36; i++) {
+	var divToInsert =  document.createElement("div");
+	container.appendChild(divToInsert);
+	divToInsert.className = "letterBox";
+	divToInsert.id = i;
+}
 
 
+
+var allTheDivs = document.getElementsByClassName("letterBox");
+var clones = 
+
+_.each(allTheDivs, function(individual, index){
+//APPLY TO ALL
+	if (individual.id < 31) {
+		individual.innerHTML = alphabet[index];
+		individual.style.color = "black";
+		individual.style.fontSize = "75px";
+		individual.style.fontFamily = "Helvetica";
+		individual.style.textAlign = "center";
+		individual.addEventListener("mouseover", function(){
+			individual.style.boxShadow = "4px 4px 2px #DDDDDD"; 
+		});
+	} else if (individual.id > 30) {
+		individual.style.backgroundColor = "#F4F4F4";
+	}
+
+//COLUMN 1
+	if (individual.id % 5 == 1) {
+		individual.addEventListener("click", function(){
+			$(individual).addClass ("growing");
+			$(individual).removeClass ("shrinking");
+		});
+		individual.addEventListener("mouseleave", function(){
+			$(individual).addClass ("shrinking");
+			$(individual).removeClass ("growing");
+			individual.style.boxShadow = "none";
+		});
+	}
+//COLUMN 2
+	
+	if (individual.id % 5 == 2) {
+		individual.addEventListener("click", function(){
+			$(individual).addClass ("growing");
+			$(individual).removeClass ("shrinking");
+		});
+		individual.addEventListener("mouseleave", function(){
+			$(individual).addClass ("shrinking");
+			$(individual).removeClass ("growing");
+			individual.style.boxShadow = "none";
+		});
+	}
+
+//COLUMN 3
+
+	if (individual.id % 5 == 3) {
+		individual.addEventListener("click", function(){
+			$(individual).addClass ("growing");
+			$(individual).removeClass ("shrinking");
+		});
+		individual.addEventListener("mouseleave", function(){
+			$(individual).addClass ("shrinking");
+			$(individual).removeClass ("growing");
+			individual.style.boxShadow = "none";
+		});
+		
+	}
+
+//COLUMN 4
+
+
+	if (individual.id % 5 == 4) {
+		individual.addEventListener("click", function(){
+			$(individual).addClass ("growing");
+			$(individual).removeClass ("shrinking");
+			var searchselector = "#" + (individual.id - 2) + ", #" + (individual.id - 1) ;
+			var removed = $(searchselector).detach();
+			removed.insertAfter($("#" + individual.id));
+		});
+		individual.addEventListener("mouseleave", function(){
+			$(individual).addClass ("shrinking");
+			$(individual).removeClass ("growing");
+			var searchselector = "#" + (individual.id - 2) + ", #" + (individual.id - 1) ;
+			var removed = $(searchselector).detach();
+			removed.insertBefore($("#" + individual.id));
+			var theOneToTheRight = "#" + (individual.id + 1);
+			var putBack = $(individual.id).detach();
+			individual.style.boxShadow = "none";
+
+			putBack.insertBefore($("#" + theOneToTheRight)); 
+
+
+		});
+
+	}
+
+//COLUMN 5
+
+	if (individual.id % 5 == 0) {
+
+		individual.addEventListener("click", function(){
+			$(individual).addClass ("growing");
+			$(individual).removeClass ("shrinking");
+			var searchselector = "#" + (individual.id - 2) + ", #" + (individual.id - 1) ;
+			var removed = $(searchselector).detach();
+			removed.insertAfter($("#" + individual.id));
+			if ($(".letterBox").hasClass("growing")) {
+				individual.addEventListener("click", function(){
+					
+				});
+	}
+
+
+		});
+
+		individual.addEventListener("mouseleave", function(){
+			$(individual).addClass ("shrinking");
+			$(individual).removeClass ("growing");
+
+			var searchselector = "#" + (individual.id - 2) + ", #" + (individual.id - 1) ;
+			var removed = $(searchselector).detach();
+			removed.insertBefore($("#" + individual.id));
+
+			var theOneToTheRight = "#" + (individual.id + 1);
+			var putBack = $(individual.id).detach();
+			individual.style.boxShadow = "none";
+
+			putBack.insertBefore($("#" + theOneToTheRight)); 
+
+
+		});
+
+	}
+
+	// if (individual.innerWidth == "56vw") {
+	// 	console.log("hello");
+	// }
+
+
+});
+
+
+	
+
+
+
+
+//animation
+
+
+
+
+// var filterSelect = document.getElementById ("filterSelect");
+// console.log(collection);
+
+
+//click, expansion, and unexpand 
+
+
+
+// function goRight () {
+// 	allTheDivs.
+// }
 

a4a735e69877c7092ebcd3e0b50edb45551be8c3
Sarah McNutt <mcnus603@newschool.edu>
2017-05-12 19:23:46 -0400

    homepage

---
+++ main.js
@@ -0,0 +1,84 @@
+	
+var collectionLabel = document.getElementById("collectionLabel");
+var wireframeLabel = document.getElementById("wireframeLabel");
+var ioProjectLabel = document.getElementById("ioProjectLabel");
+var interfaceRedesignLabel = document.getElementById("interfaceRedesignLabel");
+var filmFestivalLabel = document.getElementById("filmFestivalLabel");
+var textAdventureLabel = document.getElementById("textAdventureLabel");
+var creativeComputingLabel = document.getElementById("creativeComputingLabel");
+
+var collection = document.getElementById("collection");
+var wireframe = document.getElementById("wireframes");
+var ioProject = document.getElementById("ioProject");
+var interfaceRedesign = document.getElementById("interfaceRedesign");
+var filmFestival = document.getElementById("filmFestival");
+var textAdventure = document.getElementById("textAdventure");
+var creativeComputing = document.getElementById("creativeComputing");
+
+
+collection.addEventListener("mouseover", function() {
+	collectionLabel.style.backgroundColor = "black";
+});
+
+collection.addEventListener("mouseleave", function() {
+		collectionLabel.style.backgroundColor = "white";
+});
+
+
+
+
+
+wireframe.addEventListener("mouseover", function() {
+	wireframeLabel.style.backgroundColor = "black";
+});
+
+wireframe.addEventListener("mouseleave", function() {
+	wireframeLabel.style.backgroundColor = "white";
+});
+
+
+
+
+ioProject.addEventListener("mouseover", function() {
+	ioProjectLabel.style.backgroundColor = "black";
+});
+
+ioProject.addEventListener("mouseleave", function() {
+	ioProjectLabel.style.backgroundColor = "white";
+});
+
+filmFestival.addEventListener("mouseover", function() {
+	filmFestivalLabel.style.backgroundColor = "black";
+});
+
+filmFestival.addEventListener("mouseleave", function() {
+	filmFestivalLabel.style.backgroundColor = "white";
+});
+
+
+interfaceRedesign.addEventListener("mouseover", function() {
+	interfaceRedesignLabel.style.backgroundColor = "black";
+});
+
+interfaceRedesign.addEventListener("mouseleave", function() {
+	interfaceRedesignLabel.style.backgroundColor = "white";
+});
+
+
+textAdventure.addEventListener("mouseover", function() {
+	textAdventureLabel.style.backgroundColor = "black";
+});
+
+textAdventure.addEventListener("mouseleave", function() {
+	textAdventureLabel.style.backgroundColor = "white";
+});
+
+
+creativeComputing.addEventListener("mouseover", function() {
+	creativeComputingLabel.style.backgroundColor = "black";
+});
+
+creativeComputing.addEventListener("mouseleave", function() {
+	creativeComputingLabel.style.backgroundColor = "white";
+});
+
